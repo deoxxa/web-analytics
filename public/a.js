19493f72ba52c262727e270fff87b08c3b5fca59
deoxxa <deoxxa@fknsrs.biz>
2016-04-28 00:00:30 +1000

    shuffle around the http fallback to trigger in more cases

--- public/a.js
+++ public/a.js
@@ -13,20 +13,16 @@
 
   report('view');
 
-  try {
-    var s = new WebSocket('ws://127.0.0.1:5001/a/ws?' + qs);
+  (function ping() {
+    report('ping');
+    setTimeout(ping, 1000 * 30);
+  })();
 
-    s.addEventListener('open', function() {
-      report = function(a, v) {
-        s.send(JSON.stringify({
-          action: a,
-          vars: v || {},
-        }));
-      };
+  var ajaxMode = false;
 
-      q.forEach(function(a) { report(a[0], a[1]); });
-    });
-  } catch (e) {
+  function fallback() {
+    ajaxMode = true;
+
     report = function(a, v) {
       var xhr = new XMLHttpRequest();
       xhr.open('POST', '/a/ev?' + qs);
@@ -37,10 +33,35 @@
     };
 
     q.forEach(function(a) { report(a[0], a[1]); });
+  }
 
-    (function ping() {
-      report('ping');
-      setTimeout(ping, 1000 * 30);
-    })();
+  var fallbackTimeout = setTimeout(fallback, 1000 * 5);
+
+  try {
+    var s = new WebSocket('wss://' + document.location.host + '/a/ws?' + qs);
+
+    s.addEventListener('open', function() {
+      if (ajaxMode) {
+        return;
+      }
+
+      s.addEventListener('close', fallback);
+      s.addEventListener('error', fallback);
+
+      clearTimeout(fallbackTimeout);
+
+      report = function(a, v) {
+        s.send(JSON.stringify({
+          action: a,
+          vars: v || {},
+        }));
+      };
+
+      q.forEach(function(a) { report(a[0], a[1]); });
+    });
+  } catch (e) {
+    clearTimeout(fallbackTimeout);
+
+    fallback();
   }
 })();
